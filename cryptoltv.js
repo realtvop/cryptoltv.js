d75873b3b3ebd312babcf0caed04367f672ef13e
飞机上的长电视 <79362411+realtvop@users.noreply.github.com>
2023-08-02 22:19:55 +0800

    Update cryptoltv.js

--- cryptoltv.js
+++ cryptoltv.js
@@ -1,18 +1,22 @@
 // https://github.com/realtvop/cryptoltv.js
 
 function hmacsha256(digest, secret) {
-            const encoder = new TextEncoder();
-            const data = encoder.encode(digest);
-            const key = await crypto.subtle.importKey(
-                'raw',
-                encoder.encode(secret),
-                { name: 'HMAC', hash: { name: 'SHA-256' } },
-                false,
-                ['sign']
-            );
-            const signature = await crypto.subtle.sign('HMAC', key, data);
-            const base64String = btoa(String.fromCharCode(...new Uint8Array(signature)));
-            return base64String;
-        }
+  const encoder = new TextEncoder();
+  const data = encoder.encode(digest);
+  const keyPromise = crypto.subtle.importKey(
+    'raw',
+    encoder.encode(secret),
+    { name: 'HMAC', hash: { name: 'SHA-256' } },
+    false,
+    ['sign']
+  );
+
+  return keyPromise.then((key) => {
+    return crypto.subtle.sign('HMAC', key, data);
+  }).then((signature) => {
+    const base64String = btoa(String.fromCharCode(...new Uint8Array(signature)));
+    return base64String;
+  });
+}
 
 export default { hmacsha56 };
